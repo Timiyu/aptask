e85d18b14a06dcde1ee6755d58c1e5175c11c224
singdlmaster <singdl@outlook.com>
2022-10-18 10:40:10 +0800

    commit

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -20,7 +20,7 @@ from apps.users import views
 app_name = 'users'
 
 urlpatterns = [
-    path('register', views.Register.as_view(), name='register'),
-    path('login', views.Login.as_view(), name='login'),
-    path('logout', views.logout, name='logout')
+    path('register', views.Register.as_view(), name='user_register'),
+    path('login', views.Login.as_view(), name='user_login'),
+    path('logout', views.logout, name='user_logout')
 ]
